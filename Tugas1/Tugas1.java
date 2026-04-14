bb73bfb25c3f9ea31e805581ab055eaf35849d2d
yihahaha16 <falihaimtinan@gmail.com>
2026-04-14 14:10:08 +0700

    Add Menu, pelanggan

---
+++ Tugas1/Tugas1.java
@@ -0,0 +1,37 @@
+package Tugas1;
+
+class Menu{
+    String menuId;
+    String menuNama;
+    String menuStatus;
+    String menuKategori;
+    int menuHarga;
+
+    //Constructor untuk menginisialisasi data Menu
+    public Menu(String menuId, String menuNama, String menuStatus, String menuKategori, int menuHarga){
+        this.menuId = menuId;
+        this.menuNama = menuNama;
+        this.menuStatus = menuStatus;
+        this.menuKategori = menuKategori;
+        this.menuHarga = menuHarga;
+    }
+
+    //Method untuk menampilkan Menu
+    public void tampilMenu(){
+        System.out.println(menuId + " - " + menuNama + " - " + menuKategori + " - Rp " + menuHarga + " - " + menuStatus);
+    }
+}
+
+class Pelanggan{
+    String pelangganId;
+    String pelangganNoHP;
+    String pelangganNama;
+
+    public Pelanggan()
+
+    public void inputPelanggan()
+}
+
+public class Tugas1 {
+    
+}
