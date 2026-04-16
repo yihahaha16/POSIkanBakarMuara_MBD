2688d4ec27ba02a595075555bcaa2a5f93d5d577
ViolaineGunawan <violainegunawan06@gmail.com>
2026-04-16 22:15:00 +0700

    Interface dan Abstraction

--- Tugas1/Tugas1.java
+++ Tugas1/Tugas1.java
@@ -1,7 +1,8 @@
 package Tugas1;
 
-interface MetodePembayaran {
-    public void bayar(int total);
+//INTERFACE: digunakan untuk class yang mempunyai method sama namun logika cetak struk yang berbeda (PembayaranQRIS dan PembayaranCash)
+interface CetakStruk {
+    public void cetakStruk();
 }
 
 class Menu{
@@ -167,21 +168,76 @@ class Member extends Pelanggan{
 class Reguler extends Pelanggan{
     //bingung ape isinye
 }
-class PembayaranCash implements MetodePembayaran {
-    public void bayar(int total) {
-        System.out.println("Bayar cash: " + total);
+//ABSTRACTION: Yang disembunyikan adalah detail proses pembayaran seperti validasi saldo dan perhitungan kembalian. User hanya menggunakan method bayar() tanpa mengetahui implementasinya
+abstract class Pembayaran{
+    int total;
+    int saldo;
+    public Pembayaran(int total) { //QRIS
+        this.total = total;
+    }
+    public Pembayaran(int total, int saldo) { //Cash
+        this.total = total;
+        this.saldo = saldo;
+    }
+    public abstract void bayar();
+    public boolean cekPembayaran(int total, int saldo){
+        if(saldo<total){
+            System.out.println("Saldo tidak mencukupi");
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 }
 
-class PembayaranQRIS implements MetodePembayaran {
-    public void bayar(int total) {
-        System.out.println("Scan QRIS untuk bayar: " + total);
+class PembayaranCash extends Pembayaran implements CetakStruk{
+    public PembayaranCash(int total, int saldo) {
+        super(total,saldo);
+    }
+
+    public int hitungKembalian(){
+        return saldo - total;
+    }
+    public void bayar(){
+        System.out.println("Silahkan bayar di kasir dengan nominal Rp " + total);
+        System.out.println("Uang yang diterima Rp " + saldo);
+         if(cekPembayaran(total, saldo)){
+            System.out.println("Pembayaran berhasil");
+            System.out.println("Kembalian anda Rp " + hitungKembalian());
+            cetakStruk();
+        } else {
+            System.out.println("Uang anda kurang Rp " + (total - saldo));
+        }
+    }
+    public void cetakStruk(){
+        System.out.println("STRUK PEMBAYARAN");
+        System.out.println("Total: Rp " + total);
+        System.out.println("Bayar: Rp " + saldo);
+        System.out.println("Kembalian: Rp " + hitungKembalian());
+        System.out.println("Terima kasih!");
     }
 }
 
-class PembayaranDebit implements MetodePembayaran {
-    public void bayar(int total) {
-        System.out.println("Gesek / tap kartu debit: " + total);
+class PembayaranQRIS extends Pembayaran implements CetakStruk{
+    public PembayaranQRIS(int total, int saldo) {
+        super(total,saldo);
+    }
+
+    public void bayar(){
+        System.out.println("Silahkan scan QRIS");
+        System.out.println("Total pesanan anda Rp " + total);
+         if(cekPembayaran(total, saldo)){
+            System.out.println("Pembayaran berhasil");
+            cetakStruk();
+        } else {
+            System.out.println("Saldo anda kurang Rp " + (total - saldo));
+        }
+    }
+    public void cetakStruk(){
+        System.out.println("STRUK PEMBAYARAN QRIS");
+        System.out.println("Total: Rp " + total);
+        System.out.println("Terima kasih!");
     }
 }
 
