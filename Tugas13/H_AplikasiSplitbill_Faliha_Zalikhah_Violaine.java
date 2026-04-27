738789b82269fba91ffd2eed57f6104a2330538f
ViolaineGunawan <violainegunawan06@gmail.com>
2026-04-27 16:55:44 +0700

    Tugas 13

---
+++ Tugas13/H_AplikasiSplitbill_Faliha_Zalikhah_Violaine.java
@@ -0,0 +1,53 @@
+package Tugas13;
+
+class PembayaranSplitBill{
+    //static: digunakan untuk pajak karena pajak bersifat tetap (static)
+    static float pajak = 0.1f;
+
+    //instance: yg ada di lingkup masing-masing object
+    String[] orang; //array 1D untuk menampung orang yang ingin split bill
+    String[][] menuHarga; //array 2D untuk menampung keseluruhan menu dan harga yang dipesan
+    int[][] menuPerOrang; //array jagged untuk menampung menu(by index) yang dipesan masing-masing orang
+
+    //constructor
+     PembayaranSplitBill(String[] orang, String[][] menuHarga, int[][] menuPerOrang) {
+        this.orang = orang;
+        this.menuHarga = menuHarga;
+        this.menuPerOrang = menuPerOrang;
+    }
+
+    void hitungSplitBill() {
+        for (int i = 0; i < orang.length; i++) {
+            int total = 0;
+            for (int j = 0; j < menuPerOrang[i].length; j++) {
+                int harga = Integer.parseInt(menuHarga[menuPerOrang[i][j]][1]);//https://www.geeksforgeeks.org/java/how-to-convert-string-to-int-in-java/
+                total += harga;
+            }
+            total += (total * pajak);
+
+            System.out.println(orang[i] + " bayar: Rp " + total);
+        }
+    }
+}
+
+public class H_AplikasiSplitbill_Faliha_Zalikhah_Violaine {
+    public static void main(String[] args) {
+        PembayaranSplitBill pb = new PembayaranSplitBill(
+            new String[]{"Faliha", "Zalikhah", "Violaine"},
+            new String[][]{
+                {"Nasi Goreng", "15000"},
+                {"Ayam", "20000"},
+                {"Es Teh", "10000"},
+                {"Kelp Shake", "10000"},
+                {"Krabby Patty", "20000"}
+            },
+            new int[][]{
+                {0, 1},
+                {3, 4},
+                {0, 2}
+            }
+        );
+
+        pb.hitungSplitBill();
+    }
+}
