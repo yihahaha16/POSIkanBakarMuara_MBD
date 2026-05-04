c6b84f49595757fc85c00b50342dea88d4493643
ViolaineGunawan <violainegunawan06@gmail.com>
2026-05-04 08:18:16 +0700

    perbaiki hapus index tambah

--- project_php/config/database.php
+++ project_php/config/database.php
@@ -10,7 +10,6 @@ $username,
 $password
 );
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo "DB terhubung";
 } catch (PDOException $e) {
 die("Koneksi database gagal: " . $e->getMessage());
 }
--- project_php/public/tambah.php
+++ project_php/public/tambah.php
@@ -10,12 +10,11 @@
     require_once "..\config\database.php";
     $pesanan_id = $_GET['pesanan_id'];
     $stmt = $conn->prepare(
-        'Select dp.pesanan_id, m.menu_nama, m.menu_kategori, m.menu_harga, dp.dp_kuantitas from detail_pesanan dp join menu m on dp.menu_id = m.menu_id where dp.pesanan_id = :pesanan_id'
+        'Select dp.pesanan_id, m.menu_id, m.menu_nama, m.menu_kategori, m.menu_harga, dp.dp_kuantitas from detail_pesanan dp join menu m on dp.menu_id = m.menu_id where dp.pesanan_id = :pesanan_id'
     );
     $stmt->execute([':pesanan_id' => $pesanan_id]);
     $dp = $stmt->fetchAll(PDO::FETCH_ASSOC);
     ?>
-    <form method = "post" action = "/process/insert.php">
         <h3>Menu yang telah Anda pilih: <br></h3>
     <table border="1">
         <tr>
@@ -32,12 +31,11 @@
             <td><?=$row['menu_harga']?></td>
             <td><?=$row['dp_kuantitas']?></td>
             <td>
-                <a href="edit.php">Edit</a>
-                <a href="hapus.php">Hapus</a>
+            <button><a href="edit.php?pesanan_id=<?= $row['pesanan_id']?>&menu_id=<?= $row['menu_id'] ?>">Edit</a></button>
+            <button><a href="hapus.php?pesanan_id=<?= $row['pesanan_id']?>&menu_id=<?= $row['menu_id']?> ">Hapus</a></button>
             </td>
             </tr>
         <?php endforeach;?>
     </table>
-    </form>
 </body>
 </html>
