67ca6f68c27adcc95bfe7e63e5b273e94d757e6d
yihahaha16 <falihaimtinan@gmail.com>
2026-05-04 20:20:10 +0700

    update nama, subtotal, grandtotal

--- project_php/public/tambah.php
+++ project_php/public/tambah.php
@@ -10,11 +10,20 @@
     require_once "..\config\database.php";
     $pesanan_id = $_GET['pesanan_id'];
     $stmt = $conn->prepare(
-        'Select dp.pesanan_id, m.menu_id, m.menu_nama, m.menu_kategori, m.menu_harga, dp.dp_kuantitas from detail_pesanan dp join menu m on dp.menu_id = m.menu_id where dp.pesanan_id = :pesanan_id'
-    );
-    $stmt->execute([':pesanan_id' => $pesanan_id]);
-    $dp = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        'Select dp.pesanan_id, m.menu_id, m.menu_nama, m.menu_kategori, m.menu_harga, dp.dp_kuantitas, dp.dp_total from detail_pesanan dp join menu m on dp.menu_id = m.menu_id where dp.pesanan_id = :pesanan_id'
+        );
+        $stmt->execute([':pesanan_id' => $pesanan_id]);
+        $dp = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+    $pelanggan = $conn->prepare('select p.pelanggan_nama, ps.pelanggan_noHP from pesanan ps join pelanggan p on ps.pelanggan_noHP = p.pelanggan_noHP where ps.pesanan_id = :pesanan_id');
+    $pelanggan->execute([':pesanan_id' => $pesanan_id]);
+    $pelanggan_nama = $pelanggan->fetch(PDO::FETCH_ASSOC);
+
+    $grandtotal = $conn->prepare('select pesanan_grandtotal from pesanan where pesanan_id = :pesanan_id');
+    $grandtotal->execute([':pesanan_id'=>$pesanan_id]);
+    $pesanan_grandtotal = $grandtotal->fetch(PDO::FETCH_ASSOC);
     ?>
+        <h2>Halo <?=$pelanggan_nama['pelanggan_nama']?>, silahkan periksa kembali pesanan Anda ya!</h2>
         <h3>Menu yang telah Anda pilih: <br></h3>
     <table border="1">
         <tr>
@@ -22,6 +31,7 @@
             <th>Kategori</th>
             <th>Harga</th>
             <th>Jumlah</th>
+            <th>Subtotal</th>
             <th>Aksi</th>
         </tr>
         <?php foreach ($dp as $row): ?>
@@ -30,6 +40,7 @@
             <td><?=$row['menu_kategori']?></td>
             <td><?=$row['menu_harga']?></td>
             <td><?=$row['dp_kuantitas']?></td>
+            <td><?=$row['dp_total']?></td>
             <td>
             <button><a href="edit.php?pesanan_id=<?= $row['pesanan_id']?>&menu_id=<?= $row['menu_id'] ?>">Edit</a></button>
             <button><a href="hapus.php?pesanan_id=<?= $row['pesanan_id']?>&menu_id=<?= $row['menu_id']?> ">Hapus</a></button>
@@ -37,5 +48,8 @@
             </tr>
         <?php endforeach;?>
     </table>
+    <?php
+    ?>
+    <h4>Grantotal: Rp <?= $pesanan_grandtotal['pesanan_grandtotal'] ?></h4>
 </body>
 </html>
