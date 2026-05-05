c59b91f3da53f6e7437c11d114e58f09e7f9a284
ZalikhahKhai <chachakhairunnisa21@gmail.com>
2026-05-06 02:02:14 +0700

    update.php terbaru

--- project_php/process/update.php
+++ project_php/process/update.php
@@ -2,22 +2,20 @@
 require_once "..\config\database.php";
 try{
     $pesanan_id = $_POST['pesanan_id'];
+    $menu_id = $_POST['menu_id'];
+    $kuantitas = $_POST['kuantitas'];
 
     $stmt = $conn->prepare("
         update detail_pesanan
         set dp_kuantitas = :kuantitas
         where pesanan_id = :pesanan_id and menu_id = :menu_id
     ");
-    foreach($_POST['dp_kuantitas'] as $menu_id => $kuantitas){
-        if ($kuantitas <= 0) {
-            continue;
-        }
-        $stmt->execute([
-            ':pesanan_id' => $pesanan_id,
-            ':menu_id' => $menu_id,
-            ':kuantitas' => $kuantitas
-        ]);
-    }
+
+    $stmt->execute([
+        ':pesanan_id' => $pesanan_id,
+        ':menu_id' => $menu_id,
+        ':kuantitas' => $kuantitas
+    ]);
 
     header("Location: ../public/tambah.php?pesanan_id=" . $pesanan_id);
     exit;
