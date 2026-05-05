c468cf9dddd3b4e18ea783498a1ddf41116c4eaa
ZalikhahKhai <chachakhairunnisa21@gmail.com>
2026-05-06 01:15:10 +0700

    rapiin update.php

--- project_php/process/update.php
+++ project_php/process/update.php
@@ -1,36 +1,25 @@
 <?php
 require_once "..\config\database.php";
 try{
-    // $stmt = $conn->prepare("
-    //     update pesanan
-    //     set pelanggan_noHP = :pelanggan_noHP, pesanan_noMeja = :pesanan_noMeja, pesanan_jenis = :pesanan_jenis
-    //     where pesanan_id = :pesanan_id
-    // ");
-    // $stmt->execute([
-    //     ':pesanan_id' => $_POST['pesanan_id'],
-    //     ':pelanggan_noHP' => $_POST['pelanggan_noHP'],
-    //     ':pesanan_noMeja' => $_POST['pesanan_noMeja'],
-    //     ':pesanan_jenis' => $_POST['pesanan_jenis'],
-    // ]);
+    $pesanan_id = $_POST['pesanan_id'];
 
     $stmt = $conn->prepare("
         update detail_pesanan
-        set dp_kuantitas = :qty
+        set dp_kuantitas = :kuantitas
         where pesanan_id = :pesanan_id and menu_id = :menu_id
     ");
     foreach($_POST['dp_kuantitas'] as $menu_id => $kuantitas){
-        if (empty($pesanan_id) || empty($menu_id) || empty($kuantitas)) {
-            $errorMessage = "Semua kolom harus terisi";
-            break;
+        if ($kuantitas <= 0) {
+            continue;
         }
         $stmt->execute([
             ':pesanan_id' => $pesanan_id,
             ':menu_id' => $menu_id,
-            ':dp_kuantitas' => $kuantitas
+            ':kuantitas' => $kuantitas
         ]);
     }
 
-    header("Location: /public/tambah.php?pesanan_id=" . $pesanan_id);
+    header("Location: ../public/tambah.php?pesanan_id=" . $pesanan_id);
     exit;
 } catch (PDOException $e) {
     echo "Gagal mengubah pesanan: " . $e->getMessage();
