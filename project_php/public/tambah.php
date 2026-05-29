dda7f62e3befaad7bf52cc9cb822911311b34501
yihahaha16 <falihaimtinan@gmail.com>
2026-05-29 17:51:20 +0700

    update

--- project_php/public/tambah.php
+++ project_php/public/tambah.php
@@ -44,9 +44,9 @@ table {
     $pesanan_grandtotal = $grandtotal->fetch(PDO::FETCH_ASSOC);
     ?>
     <div style="margin-top:7px;margin-bottom:0px;text-align:center">
-        <h2>Halo <?=$pelanggan_nama['pelanggan_nama']?>, silahkan periksa kembali pesanan Anda.</h2>
+        <h2>Pesanan atas nama: <?=$pelanggan_nama['pelanggan_nama']?>, harap periksa kembali pesanan.</h2>
     </div>
-        <h3>Menu yang telah Anda pilih:</h3>
+        <h3>Menu yang telah pilih:</h3>
     <table border="1">
         <tr>
             <th>Nama</th>
