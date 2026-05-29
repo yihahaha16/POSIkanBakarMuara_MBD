175d55c515b55dc56ec95117e7cf326102327c42
ViolaineGunawan <violainegunawan06@gmail.com>
2026-05-29 09:24:53 +0700

    menyesuaikan tampilan save pesanan

--- project_php/public/savepesanan.php
+++ project_php/public/savepesanan.php
@@ -21,7 +21,7 @@ table {
     <div style="text-align:center;background:#f44236;width:530px;height:100px;border-radius:20px 20px 0 0;color:white">    
     <h1 style="margin-top:15px;margin-bottom: 0px">Sistem Pemesanan </h1><h1 style="margin-top: 0px"> Ikan Bakar Muara</h1>
     </div> 
-<table style="width:470px">
+<div style="background:#f9f9f9;width:530px;border-radius:0px 0px 20px 20px;padding:30px;box-sizing:border-box;text-align:center;">
     <?php 
     session_start();
     require_once "..\config\database.php";
@@ -34,5 +34,6 @@ table {
     Mohon tunggu hidangan Anda, Silahkan proses pembayaran di Kasir <br><br>
     <?php unset($_SESSION['pesanan_id'])?>
     <a href="index.php"><button style="background-color:#f44236;border:2px solid #f44236;border-radius:5px;padding:3px;width: 100px;color:white;cursor: pointer">Kembali ke Beranda</button></a>
-</table>
+</div>
+    </body>
 </html>
